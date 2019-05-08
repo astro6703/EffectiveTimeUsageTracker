4cec6b0405db086d140a793381c9cad1d5260c57
astro6703 <astro6703@gmail.com>
2019-05-08 09:32:15 +0300

    Added HomeController with action available only to authorised users

--- EffectiveTimeUsageTracker/Controllers/HomeController.cs
+++ EffectiveTimeUsageTracker/Controllers/HomeController.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EffectiveTimeUsageTracker.Models;
+using EffectiveTimeUsageTracker.Models.Objectives;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EffectiveTimeUsageTracker.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserObjectivesRepository _repository;
+
+        public HomeController(UserManager<ApplicationUser> userManager, IUserObjectivesRepository repository)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException($"{nameof(userManager)} was null");
+            _repository  = repository ?? throw new ArgumentNullException($"{nameof(repository)} was null");
+        }
+
         [Authorize]
         public IActionResult Index()
         {
