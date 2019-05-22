7ebbb3b8e992505fe0aced3890c7c4a757ee241f
astro6703 <astro6703@gmail.com>
2019-05-22 09:16:48 +0300

    Implemented extension methods for adding and updating enumerable

---
+++ EffectiveTimeUsageTracker/MyExtensionMethods/MyExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using EffectiveTimeUsageTracker.Models.Objectives;
+using System.Collections.Generic;
+
+namespace EffectiveTimeUsageTracker.MyExtensionMethods
+{
+    public static class MyExtensionMethods
+    {
+        public static IEnumerable<Objective> UpdateObjectiveStatus(this IEnumerable<Objective> enumerable, Objective objective)
+        {
+            foreach (var item in enumerable)
+            {
+                if (item.Name != objective.Name)
+                    yield return item;
+
+                else yield return objective;
+            }
+        }
+
+        public static IEnumerable<Objective> AddOneObjective(this IEnumerable<Objective> enumerable, Objective objective)
+        {
+            foreach (var item in enumerable)
+                yield return item;
+
+            yield return objective;
+        }
+    }
+}
